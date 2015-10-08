1c2670074c9746667e15dae49099bd8c37eea6ee
AndresSV <andres-salas@hotmail.com>
2015-10-08 09:45:20 -0500

    Update Quiz #06 .cpp

--- Quiz #06 .cpp
+++ Quiz #06 .cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
+
+long superpower(long a, long b){
+  long superpower = 0;
+  superpower = pow(a,b);
+  return superpower;
+}
 
 void stars(int iStars){
   for (int i = 1 ; i <= iStars; i++){
