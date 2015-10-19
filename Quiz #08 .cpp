b691606991714f4204b790a92b449eb9f35cdf3c
AndresSV <andres-salas@hotmail.com>
2015-10-19 09:45:09 -0500

    Update Quiz #08 .cpp

--- Quiz #08 .cpp
+++ Quiz #08 .cpp
@@ -3,27 +3,28 @@
 
 using namespace std;
 
-int sumSquares_List(int x){
-    int number = 0;
+int sumSquares_List(vector<int> x){
+
     int sum = 0;
     int result = 0;
-    vector<int>  vector1;
-    for (int i = 0; i < x; i++){
-        cout << "Input first number: ";
-        cin >> number;
-        vector1.push_back(number);
-    }
-    for (int i = 0; i < x; i++){
-        result = vector1[i] * vector1[i];
+    for (int i = 0; i < x.size(); i++){
+        result = x[i] * x[i];
         sum += result;
     }
     return sum;
 }
 
 int main(){
-    int size;
+    int size = 0;
+    int number = 0;
+    vector<int>  vector1;
     cout << "Input the size: " << endl;
     cin >> size;
-    cout << "Result is: " << sumSquares_List(size) << endl;
+    for (int i = 0; i < size; i++){
+        cout << "Input " << i + 1  <<" number: ";
+        cin >> number;
+        vector1.push_back(number);
+    }
+    cout << "Result is: " << sumSquares_List(vector1) << endl;
     return 0;
 }
