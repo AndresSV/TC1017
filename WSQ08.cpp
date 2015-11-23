c577a176a4204b15bddde0d55f2ac126d62903f4
AndresSV <andres-salas@hotmail.com>
2015-11-23 13:25:18 -0600

    Create WSQ08.cpp

---
+++ WSQ08.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int sum(int x, int y){
+  int z;
+  z = x + y;
+  return z;
+}
+
+int diference(int x, int y){
+  int z;
+  z = x-y;
+  return z;
+}
+
+int product(int x, int y){
+  int z;
+  z = x*y;
+  return z;
+}
+
+int division(int x, int y){
+  int z;
+  z = x/y;
+  return z;
+}
+
+float remainder(int x, int y){
+  int z;
+  z = x%y;
+  return z;
+}
+
+int main(){
+
+  return 0;
+}
